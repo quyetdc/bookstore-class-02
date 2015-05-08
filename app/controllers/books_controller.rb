160281050086cf9043d6e2c95551516cbca85a6a
quyetdc <quyetdc@evolableasia.vn>
2015-05-08 17:01:37 +0700

    Add route and prototype image for books#show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,11 @@ class BooksController < ApplicationController
   # Get /books
   # Currently set root path to this
   def index
-
   end
+
+  # Get /books/:id
+  def show
+    @book = Book.where(id: params[:id]).first
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'books#index'
 
+  get 'books/:id' => "books#show"
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
